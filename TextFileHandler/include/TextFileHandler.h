d8340b699eafbb20d84cfa02fbe3e918f2daa530
leonf08 <leonf008@gmail.com>
2023-02-09 23:26:19 +0100

    implemented TextFileHandler

---
+++ TextFileHandler/include/TextFileHandler.h
@@ -0,0 +1,7 @@
+#ifndef TEXTFILEHANDLER_H
+#define TEXTFILEHANDLER_H
+
+
+int HandleTextFile(const char filename[], int *hashmap);
+
+#endif /* TEXTFILEHANDLER_H */
--- TextFileHandler/src/TextFileHandler.c
+++ TextFileHandler/src/TextFileHandler.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "TextFileHandler.h"
 
-void HandleTextFile(const char filename[])
+
+int HandleTextFile(const char filename[], int *hashmap)
 {
     FILE *filePtr = fopen(filename, "r");
-
-    while (filePtr != EOF)
-    {
-        int c = fgetc(filePtr);
+    if (filePtr == NULL) {
+        perror("Error occured");
+        return 1;
     }
+
+    int c;
+    while ((c = fgetc(filePtr)) != EOF)
+    {
+        hashmap[c]++;
+    }
+
+    return 0;
 }
